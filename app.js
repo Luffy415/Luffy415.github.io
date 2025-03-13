910d392eb46b9381a3ac3fe6bda3e76231677e7d
Luffy415 <divyanshulawda@gmail.com>
2025-03-13 18:56:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
 let chk = false
 
+window.addEventListener("load", (event) => {
+     new cursoreffects.followingDotCursor({color: ["#A3B9D1"]});
+});
+
 function show() {
      hover.classList.add('active');
      modal.classList.add('show');
@@ -30,3 +34,78 @@ function toogle() {
           sidebar.style.animation = 'fadeOut 0.25s linear forwards'
      }
 }
+
+class TextScramble {
+     constructor(el) {
+       this.el = el
+       this.chars = '!<>-_\\/[]{}—=+*^?#________'
+       this.update = this.update.bind(this)
+     }
+     setText(newText) {
+       const oldText = this.el.innerText
+       const length = Math.max(oldText.length, newText.length)
+       const promise = new Promise((resolve) => this.resolve = resolve)
+       this.queue = []
+       for (let i = 0; i < length; i++) {
+         const from = oldText[i] || ''
+         const to = newText[i] || ''
+         const start = Math.floor(Math.random() * 40)
+         const end = start + Math.floor(Math.random() * 40)
+         this.queue.push({ from, to, start, end })
+       }
+       cancelAnimationFrame(this.frameRequest)
+       this.frame = 0
+       this.update()
+       return promise
+     }
+     update() {
+       let output = ''
+       let complete = 0
+       for (let i = 0, n = this.queue.length; i < n; i++) {
+         let { from, to, start, end, char } = this.queue[i]
+         if (this.frame >= end) {
+           complete++
+           output += to
+         } else if (this.frame >= start) {
+           if (!char || Math.random() < 0.28) {
+             char = this.randomChar()
+             this.queue[i].char = char
+           }
+           output += `<span class="dud">${char}</span>`
+         } else {
+           output += from
+         }
+       }
+       this.el.innerHTML = output
+       if (complete === this.queue.length) {
+         this.resolve()
+       } else {
+         this.frameRequest = requestAnimationFrame(this.update)
+         this.frame++
+       }
+     }
+     randomChar() {
+       return this.chars[Math.floor(Math.random() * this.chars.length)]
+     }
+   }
+   
+   // ——————————————————————————————————————————————————
+   // Example
+   // ——————————————————————————————————————————————————
+   
+   const phrases = [
+     'shaurya'
+   ]
+   
+   const el = document.querySelector('.name')
+   const fx = new TextScramble(el)
+   
+   let counter = 0
+   const next = () => {
+     fx.setText(phrases[counter]).then(() => {
+       setTimeout(next, 800)
+     })
+     counter = (counter + 1) % phrases.length
+   }
+   
+   next()
